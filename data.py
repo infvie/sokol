125d3be6ad2a7d3aa5549318e0cef37aa08709bb
infvie <sreenath.raparti@gmail.com>
2018-12-20 21:18:18 -0500

    what should be collected? :/

--- data.py
+++ data.py
@@ -1,9 +1,24 @@
 import requests
 import pandas as pd
+import xml.etree.ElementTree as ET
+import sqlite3 as sql
 
 def main():
-    text = requests.get('http://gd2.mlb.com/components/game/mlb/year_2018/month_06/day_08/gid_2018_06_08_anamlb_minmlb_1/inning/inning_2.xml')
-    print(text)
+    # navigate to all pages in the season
+    f = open('inning_all.xml')
+
+    # parse the xml files
+    tree = ET.parse(f)
+
+    # create collector object 
+    atbat = pd.DataFrame
+    play = pd.DataFrame
+
+    # go through atbat and create a unique key  
+    # for atbat_obj in tree.
+
+    # pd.DataFrame.from_dict(t,orient='columns')
+    # pd.DataFrame.from_dict(t,orient='columns')
 
 if __name__ == '__main__':
     main()
